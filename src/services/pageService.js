0c371ff0b708fc70f98a5414c54a4338932be29d
nattababi <nataly.babicheva@gmail.com>
2020-07-09 10:53:51 -0400

    modify project

--- src/services/pageService.js
+++ src/services/pageService.js
@@ -1,6 +1,6 @@
 import http from "./httpService";
 
 export async function getPage(page) {
-  const { data: pageObj } = await http.get("pages/" + page);
+  const { data: pageObj } = await http.get("pages/" + page); 
   return pageObj;
 }
